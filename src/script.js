ab7f0d3b7823fd731375a3e57ac7c176907d3575
SerhioAndros <androsiuksv@gmail.com>
2021-05-12 01:01:10 +0300

    HW8 finish

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const refs = {
     galleryNode: document.querySelector(".js-gallery"),
     modalNode: document.querySelector(".js-lightbox"),
     modalOverlay: document.querySelector(".lightbox__overlay"),
-    modalImage: document.querySelector(".lightbox__image"),
+    // modalImage: document.querySelector(".lightbox__image"),
     modalBtn: document.querySelector(".lightbox__button"),
     modalImgWrapper: document.querySelector(".lightbox__content"),
 };
@@ -20,7 +20,7 @@ galleryItems.forEach(img => {
     aNode.href = img.original;
     liNode.append(aNode);
 
-    aNode.innerHTML = `<img class="gallery__image" src= ${img.preview} data-source=${img.original} alt=${img.description}/>`;
+    aNode.innerHTML = `<img class="gallery__image" src= ${img.preview} id=${galleryItems.indexOf(img)} data-source=${img.original} alt=${img.description}/>`;
     
     galleryArr.push(liNode);
 });
@@ -31,35 +31,64 @@ refs.galleryNode.addEventListener('click', galleryOpen);
 
 refs.modalNode.addEventListener('click', galleryCloseClick);
 
+const galleryLength = refs.galleryNode.querySelectorAll('li').length;
+
+let currentImgIndex = "";
 
 function galleryOpen(evt) {
-    if (evt.target.nodeName !== "IMG") {
-        return
+    if (evt.target.nodeName === "IMG") {
+        evt.preventDefault();
+        currentImgIndex = evt.target.id;
+        window.addEventListener('keydown', galleryCloseKey);
+        window.addEventListener('keydown', galleryChange);
+        refs.modalNode.classList.add("is-open");
+        refs.modalImgWrapper.innerHTML = `<img class="lightbox__image" src="${evt.target.dataset.source}" alt="${evt.target.getAttribute("alt")}" />`;
     };
-    window.addEventListener('keydown', galleryCloseKey);
-    window.addEventListener('keydown', galleryChange);
-    evt.preventDefault();
-    refs.modalNode.classList.add("is-open");
-    refs.modalImgWrapper.innerHTML = `<img class="lightbox__image" src="${evt.target.dataset.source}" alt="${evt.target.getAttribute("alt")}" />`;
 };
 
 function galleryChange(evt) {
     if (evt.code === "ArrowRight") {
-        // const currentImg = refs.modalImage;
-        // console.log(currentImg);
-        // refs.modalImage.src = "321";
-        // refs.modalImage.alt = "321";
-        console.log("swipe to the right")
+        if (currentImgIndex < (galleryLength - 1)) {
+            let nextImgIndex = +currentImgIndex + 1;
+            let nextImg = document.getElementById(nextImgIndex);
+            currentImgIndex++;
+            refs.modalImgWrapper.innerHTML = `<img class="lightbox__image" src="${nextImg.dataset.source}" alt="${nextImg.getAttribute("alt")}" />`;
+            console.log("go to the right");
+        }
+        // else if (currentImgIndex === (galleryLength)) {
+        //     currentImgIndex = 0;
+        //     let nextImgIndex = +currentImgIndex;
+        //     let nextImg = document.getElementById(nextImgIndex);
+        //     refs.modalImgWrapper.innerHTML = `<img class="lightbox__image" src="${nextImg.dataset.source}" alt="${nextImg.getAttribute("alt")}" />`;
+        //     console.log("go to the right");
+        // };
     }
     else if (evt.code === "ArrowLeft") {
-        console.log("turn left")
+        if (currentImgIndex > 0) {
+            let nextImgIndex = +currentImgIndex - 1;
+            let nextImg = document.getElementById(nextImgIndex);
+            currentImgIndex--;
+            refs.modalImgWrapper.innerHTML = `<img class="lightbox__image" src="${nextImg.dataset.source}" alt="${nextImg.getAttribute("alt")}" />`;
+            console.log("turn to the left")
+        }
+        // else if (currentImgIndex === 1) {
+            
+        //     currentImgIndex = galleryLength;
+        //     let nextImgIndex = +currentImgIndex;
+        //     let nextImg = document.getElementById(nextImgIndex);
+        //     currentImgIndex--;
+        //     refs.modalImgWrapper.innerHTML = `<img class="lightbox__image" src="${nextImg.dataset.source}" alt="${nextImg.getAttribute("alt")}" />`;
+        //     console.log("go to the right");
+        // }
+        
     }
     else return
 };
 
 function closeModal() {
     refs.modalNode.classList.remove("is-open");
-    refs.modalImgWrapper.innerHTML = "";
+    refs.modalImgWrapper.innerHTML = '<img class="lightbox__image" src="" alt="" />';
+    currentImgIndex = "";
 }
 
 function galleryCloseClick(evt) {
